9bc7c8bbfe0a3ff3a71df9378a084ce2e9f2bc5c
Pyeong won Lee <154335742+lpw0625@users.noreply.github.com>
2025-12-11 21:44:01 +0900

    2025_12_11_변수

--- Program/source.c
+++ Program/source.c
@@ -1,11 +1,112 @@
-// 헤더 파일 
-// # include <stdio.h> 입력한 파일을 출력해주는데 도와주는 프로그램.
-
-// main 함수 
 
 #include <stdio.h>
 
 void main()
 {
-	printf("Hello World");
+#pragma region 변수
+
+// 데이터를 저장할 수 있는 메모리 공간을 생성하는 것.
+
+#pragma region 자료형
+	// 데이터를 저장하기 위해 데이터의 형태를 정해주는 것
+
+	// 자료형은 각각의 자료형마다 크기가 정해져 있으며 자료형의 
+	// 크기는 바이트 단위로 이루어져 있다. 자료형의 경유 자료형에
+	// 따라 저장할 수 있는 값의 종류와 범위가 결정된다.
+
+
+#pragma endregion
+
+#pragma region 변수의 이름 규칙
+
+	// 1. 변수의 이름은 대소문자를 구분한다
+
+	// ex)int HEALTH;
+	// ex) int health;
+
+// 변수의 이름으로 예약어를 사용할 수 없다
+
+// ex) char char)
+
+// 3. 변수의 이름에는 공백이 포함될 수 없다.
+// ex) float time Delta;
+
+// 4. 변수의 이름으로 특수 문자 "_", "$" 만 사용할 수 있다.
+
+	long currency$;
+	short under_score;
+	
+// 5. 변수의 이름은 숫자로 시작할 수 없다.
+
+	//ex) int year2025;
+	//ex) int 5day;
+
+
+#pragma region 서식 지정자
+
+	// 변수의 값을 출력하기 위한 자료형의 정보를 명시적으로
+	// 지정해주는 것입니다.
+
+	// 서식 지정자의 경우 하나의 표준 출력 함수에 여러 개의
+	// 서식을 넣을 수 있으며, 서식 지정자의 순서는 왼쪽에서
+	// 부터 시작이 된다
+
+
+	// 서식 지정자는 각각의 서식에 따라 원하는 데이터를 출력
+	// 할 수 있으며, 서식과 변수의 형태가 일치하지 않으면
+	// 원하는 값이 출력되지 않는다.
+
+	char character = 'A';
+	int atk = 10;
+	int def = 20;
+	int speed = 15;
+	int hp = 500;  
+
+	float increaseExp = 3.5f;
+
+	printf("character : %c\n", character);
+	printf("atk : %d\n", atk);
+	printf("def : %d\n", def);
+	printf("spped : %d\n", speed);
+	printf("hp : %d\n", hp);
+	printf("레벨 당 증가 EXP : %f", increaseExp);
+
+	/*printf("character : %c \natk : %d \n레벨 당 경험치 증가량 : %f", character, atk, increaseExp);*/
+
+
+
+
+#pragma endregion
+
+
+	
+
+
+
+
+
+
+#pragma endregion
+
+
+
+
+		
+
+
+
+
+
+
+
+#pragma endregion
+
+
+
+
+
+
+
+
 }
+
